2fec42d4e69332748771fc0a0fc8ff87bada59eb
Kelly Porowski <kellyporowski@Kellys-MacBook-Pro.local>
2018-01-18 20:49:27 +0100

    adding folders

--- scripts/script.js
+++ scripts/script.js
@@ -6,24 +6,27 @@ var lowOrHi = document.querySelector('.lowOrHi');
 
 var guessSubmit = document.querySelector('.guessSubmit');
 var guessField = document.querySelector('.guessField');
-
 var guessCount = 1;
 var resetButton;
 
+guessField.focus();
+
 function checkGuess() {
   var userGuess = Number(guessField.value);
-  if (guessCount === 1) {
+  if(guessCount === 1) {
     guesses.textContent = 'Previous guesses: ';
   }
   guesses.textContent += userGuess + ' ';
- 
-  if (userGuess === randomNumber) {
+
+
+  if(userGuess === randomNumber) {
     lastResult.textContent = 'Congratulations! You got it right!';
     lastResult.style.backgroundColor = 'green';
     lowOrHi.textContent = '';
     setGameOver();
-  } else if (guessCount === 10) {
+  } else if(guessCount === 10) {
     lastResult.textContent = '!!!GAME OVER!!!';
+    lowOrHi.textContent = '';
     setGameOver();
   } else {
     lastResult.textContent = 'Wrong!';
@@ -34,14 +37,15 @@ function checkGuess() {
       lowOrHi.textContent = 'Last guess was too high!';
     }
   }
- 
+
   guessCount++;
   guessField.value = '';
   guessField.focus();
 }
- guessSubmit.addEventListener('click', checkGuess);
- 
- function setGameOver() {
+
+guessSubmit.addEventListener('click', checkGuess);
+
+function setGameOver() {
   guessField.disabled = true;
   guessSubmit.disabled = true;
   resetButton = document.createElement('button');
@@ -54,7 +58,7 @@ function resetGame() {
   guessCount = 1;
 
   var resetParas = document.querySelectorAll('.resultParas p');
-  for (var i = 0 ; i < resetParas.length ; i++) {
+  for(var i = 0 ; i < resetParas.length ; i++) {
     resetParas[i].textContent = '';
   }
 
